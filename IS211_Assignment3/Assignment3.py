b6b19c940a41d0fc95eb6499d2540586bcce2df4
Alex Waters <alexwad12@gmail.com>
2025-09-10 23:47:58 -0400

    5th Images and most popular browser

--- IS211_Assignment3/Assignment3.py
+++ IS211_Assignment3/Assignment3.py
@@ -19,51 +19,76 @@ def download_data(url):
     return response.read().decode('utf-8')
 
 
-def process_csv(weblog_data):
+def read_csv(weblog_data):
     """
-    Processes the CSV file, and returns a count of total rows, and total image files.
+    Processes the CSV file, and returns a list of dictionaries.
 
     :param weblog_data: str
     :return: count of total rows and total image files [dict]
     """
-    image_extensions: list = ['jpg', 'gif', 'png']
-    pattern = r'\.(' + '|'.join(image_extensions) + ')$'
-    image_count: int = 0
-    total_rows: int = 0
-
+    records = []
     with io.StringIO(weblog_data) as file:
         reader = csv.reader(file, delimiter=',')
         column_names: list = ['path', 'datetime_accessed', 'browser', 'request', 'size']
 
         for row in reader:
             record = dict(zip(column_names, row))
-            total_rows += 1
+            records.append(record)
 
-            if re.search(pattern, record['path'], re.IGNORECASE):
-                image_count += 1
-
-    return {'image_count': image_count, 'total_rows': total_rows}
+    return records
 
 
-def find_image_percentage(image_count: int, total_rows: int):
+def count_images(records):
     """
-    Checks for .jpg, .gif, .png files extensions in web search hits and returns
-    the percentage of images found. 
+    Checks for .jpg, .gif, .png files extensions in the given dataset.
 
-    :return: Image requests account for (x)% of all requests
+    Args: records: list of dictionaries
+
+    :return: count of total image files [int]
     """
-    percentage: float = (image_count / total_rows) * 100
+    image_extensions: list = ['jpg', 'gif', 'png']
+    pattern = r'\.(' + '|'.join(image_extensions) + ')$'
+    image_count: int = 0
+
+    for record in records:
+        if re.search(pattern, record['path'], re.IGNORECASE):
+            image_count += 1
+
+    return image_count
+
+def find_image_percentage(image_count: int, rows_total: int):
+    """
+    Checks for .jpg, .gif, .png files extensions in the given dataset.
+
+    :return: Percentage of total image files [float].
+    """
+    percentage: float = (image_count / rows_total) * 100
     return percentage
 
-def find_most_used_browser(weblog_data):
+def find_most_used_browser(records):
     """
     Returns the name of the most used browser in the data set.
 
-    Args:
+    Args: records: list of dictionaries
 
     :return: "The most popular browser in the data set was [Firefox, Chrome, Internet Explorer or Safari]."
     """
 
+    browser_types: list = ['Firefox', 'Chrome', 'Internet Explorer', 'Safari']
+    pattern = r'(' + '|'.join(browser_types) + ')'
+    browser_counts: dict = {}
+
+    for browser in browser_types:
+        browser_counts[browser] = 0
+
+    for record in records:
+        match = re.search(pattern, record['browser'])
+        if match:
+            found_browser = match.group(1)  # Get the actual browser that was found
+            browser_counts[found_browser] += 1  # Increment the correct browser
+
+    most_popular = max(browser_counts, key=browser_counts.get)
+    return most_popular
 
 
 def main():
@@ -93,15 +118,15 @@ def main():
         print("Error downloading data: {}".format(str(e)))
         sys.exit(1)  # Exit with error code
 
-    # Process csv data
-    results = process_csv(weblog_data)
-    image_count = results['image_count']
-    total_rows = results['total_rows']
-
-    percentage = find_image_percentage(image_count, total_rows)
+    records = read_csv(weblog_data)
+    image_count = count_images(records)
+    total_rows = len(records)
+    image_percentage = find_image_percentage(image_count, total_rows)
+    browser_count = find_most_used_browser(records)
 
     print(f"Found {image_count} images out of {total_rows} total.")
-    print(f"Image request account for {percentage:.2f}% of all requests in data.")
+    print(f"Image request account for {image_percentage:.2f}% of all requests in data.")
+    print(f"Most used browser was {browser_count}.")
 
 
 if __name__ == '__main__':
